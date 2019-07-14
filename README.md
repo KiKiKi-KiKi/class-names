efa00a991d1701791635f8fdddaf4c4fea0afdf6
KiKiKi <chaika.design@gmail.com>
2019-07-14 16:57:17 +0900

    Add tests

--- README.md
+++ README.md
@@ -2,11 +2,103 @@
 
 ## useage
 
+`classnames()` return merged arguments.
+
 ```js
 import classnames from 'class-names';
 
+classnames('foo');
+// => "foo"
+
 classnames('foo', 'bar');
-// => 'foo bar'
+// => "foo bar"
+
 classnames('foo', 'bar', 'baz');
-// => 'foo bar baz'
+// => "foo bar baz"
+
+classnames('foo', 'bar', 'foo');
+// => "foo bar"
+```
+
+`classnames()` return Number to String
+```js
+classnames(1, 0, -1, 1.2, -2.3);
+// => "1 0 -1 1.2 -2.3"
+```
+
+`classnames()` ignore `null`, `undefined`, `NaN`, `Boolean`
+```js
+classnames(null, undefined, NaN, true, false);
+// => ""
+```
+
+### Array arguments
+
+classnames map array value to class name strings
+```js
+classnames([]);
+// => ""
+
+classnames(['foo', 'bar', 'baz']);
+// => "foo bar baz"
+
+classnames('foo', ['bar', 'baz', 'foo']);
+// => "foo bar baz"
+
+classnames(['foo', 0, 'bar', null], ['baz', 1, 'foo']);
+// => "foo 0 bar baz 1"
+```
+
+Nested Array arguments
+```js
+classnames(['foo', ['bar', 'baz'], 'bar']);
+// => "foo bar baz"
+
+classnames(['foo', ['bar', 'baz'], 'bar'], [0, [1, 3], 2, 3]);
+// => "foo bar baz 0 1 3 2"
+```
+
+### Object arguments
+
+`classnames` return true values key as a class name.
+```js
+classnames({
+  foo: true,
+  bar: false,
+  baz: true,
+});
+// => "foo baz"
+
+classnames('baz', {
+  foo: true,
+  bar: false,
+  baz: true
+});
+// => "baz foo"
+
+classnames(['foo', 'bar', {
+  foo: true,
+  bar: false,
+  baz: true
+});
+// => "foo bar baz"
+```
+
+When argument is Function (Object).
+```js
+classnames( function() {return "foo"} );
+// => ""
+
+classnames( (function() { return "foo" })() );
+// => "foo"
+```
+
+When argument is RegExp Object.
+```js
+classnames(/ab+c/i);
+// => ""
+
+const regex = new RegExp(/ab+c/, 'i');
+classnames(regex);
+// => ""
 ```
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,124 @@
 import classnames from '../src/';
 
-test('foo return foo', () => {
-  expect( classnames('foo') ).toBe('foo');
+describe('classnames return merged arguments', () => {
+  describe('Empty arguments', () => {
+    test('classnames() return ""', () => {
+      expect( classnames() ).toBe('');
+    });
+  });
+
+  test('classnames("foo") return "foo"', () => {
+    expect( classnames('foo') ).toBe('foo');
+  });
+
+  test('classnames("foo", "bar") return "foo bar"', () => {
+    expect( classnames('foo', 'bar') ).toBe('foo bar');
+  });
+
+  test('classnames("foo", "bar", "baz") return "foo bar baz"', () => {
+    expect( classnames('foo', 'bar', 'baz') ).toBe('foo bar baz');
+  });
+
+  test('classnames("foo", "bar", "foo") return "foo bar"', () => {
+    expect( classnames('foo', 'bar', 'foo') ).toBe('foo bar');
+  });
+});
+
+describe('classnames return Number to String', () => {
+  test('classnames(1, 0 -1, 1.2, -2.3) return "1 0 -1 1.2 -2.3"', () => {
+    expect( classnames(1, 0, -1, 1.2, -2.3) ).toBe('1 0 -1 1.2 -2.3');
+  });
+});
+
+describe('classnames ignore null, undefined, NaN, boolean', () => {
+  test('classnames(null, undefined, NaN, true, false) return ""', () => {
+    expect( classnames(null, undefined, NaN, true, false) ).toBe('');
+  });
+});
+
+describe('Array arguments', () => {
+  describe('Empty Arraty', () => {
+    test('classnames([]) return ""', () => {
+      expect( classnames([]) ).toBe('');
+    });
+  });
+
+  describe('classnames map array value to class name strings', () => {
+    test('classnames(["foo", "bar", "baz"]) return "foo bar baz"', () => {
+      expect( classnames(['foo', 'bar', 'baz']) ).toBe('foo bar baz');
+    });
+
+    test('classnames("foo", ["bar", "baz", "foo"]) return "foo bar baz"', () => {
+      expect( classnames('foo', ['bar', 'baz', 'foo']) ).toBe('foo bar baz');
+    });
+
+    test('classnames(["foo", 0, "bar", null], ["baz", 1, "foo"]) return "foo 0 bar baz 1"', () => {
+      expect( classnames(['foo', 0, 'bar', null], ['baz', 1, 'foo']) ).toBe('foo 0 bar baz 1');
+    });
+  });
+
+  describe('Nested Array', () => {
+    test('classnames(["foo", ["bar", "baz"], "bar"]) return "foo bar baz"', () => {
+      expect( classnames(['foo', ['bar', 'baz'], 'bar']) ).toBe('foo bar baz');
+    });
+
+    test('classnames(["foo", ["bar", "baz"], "bar"], [0, [1, 3], 2, 3]) return "foo bar baz 0 1 3 2"', () => {
+      expect( classnames(['foo', ['bar', 'baz'], 'bar'], [0, [1, 3], 2, 3]) ).toBe('foo bar baz 0 1 3 2');
+    })
+  })
+});
+
+describe('Object arguments', () => {
+  describe('Empty Object', () => {
+    test('classnames({}) return ""', () => {
+      expect( classnames({}) ).toBe('');
+    });
+  });
+
+  describe('Return true values key as a class name', () => {
+    test('classnames({foo: true, bar: false, baz: true}) return "foo baz"', () => {
+      expect( classnames({
+        foo: true,
+        bar: false,
+        baz: true,
+      }) ).toBe('foo baz');
+    });
+
+    test('classnames("baz", {foo: true, bar: false, baz: true}) return "baz foo"', () => {
+      expect( classnames('baz', {
+        foo: true,
+        bar: false,
+        baz: true
+      }) ).toBe('baz foo');
+    });
+
+    test('classnames(["foo", "bar"], {foo: true, bar: false, baz: true}) return "foo bar baz', () => {
+      expect( classnames(['foo', 'bar'], {
+        foo: true,
+        bar: false,
+        baz: true
+      }) ).toBe('foo bar baz');
+    });
+  });
+
+  describe('Function, RegExp Object', () => {
+    test('classnames(function() { return "foo" }) return ""', () => {
+      expect( classnames( function() {return "foo"} ) ).toBe("");
+    });
+
+    test('classnames((function() { return "foo" })()) return "foo"', () => {
+      expect( classnames( (function() { return "foo" })() ) ).toBe("foo");
+    });
+
+    describe('ignore RegExp Object', () => {
+      test('classnames(/ab+c/i) return ""', () => {
+        expect( classnames(/ab+c/i) ).toBe('');
+      });
+
+      test('Argument is a new RegExp Object', () => {
+        const regex = new RegExp(/ab+c/, 'i');
+        expect( classnames(regex) ).toBe('');
+      });
+    });
+  });
 });
